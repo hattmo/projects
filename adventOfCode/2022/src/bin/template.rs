53cd21378e3ddab260701ef5432ad0430735615b
Matthew Howard <matthew@hattmo.com>
2022-12-20 14:16:34 -0500

    advent of code solutions

--- adventOfCode/2022/src/bin/template.rs
+++ adventOfCode/2022/src/bin/template.rs
@@ -20,18 +20,18 @@ fn part_2(input: &str) -> i32 {
 mod test {
     use crate::{part_1, part_2};
 
-    const TEST_DATA: &str = "TEST_DATA";
+    const TEST_DATA: &str = r#"TEST_DATA"#;
     #[test]
     fn test1() {
         let expected = 0;
         let actual = part_1(TEST_DATA);
-        assert_eq!(expected, actual)
+        assert_eq!(expected, actual);
     }
 
     #[test]
     fn test2() {
         let expected = 0;
         let actual = part_2(TEST_DATA);
-        assert_eq!(expected, actual)
+        assert_eq!(expected, actual);
     }
 }
