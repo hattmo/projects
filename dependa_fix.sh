956399b5c6aec2a0d030883cd27997c0a3418e5e
Matthew Howard <matthew@hattmo.com>
2023-11-30 23:49:06 -0500

    add script to do dependabot fixes

---
+++ dependa_fix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for BRANCH in $(git branch -a | grep origin/dependabot | cut -d '/' -f 3-)
+do
+    git switch $BRANCH
+    git rebase main
+    if [ $? -ne 0 ]; then
+        echo "Rebase failed for $BRANCH. resolve conflicts and run 'git rebase --continue'"
+        read -p "Press enter when done"
+    fi
+    git push --force
+    git switch main
+    git merge $BRANCH
+    git push -d origin $BRANCH
+    git branch -d $BRANCH
+done
+git rebase -i origin/main
+git push
