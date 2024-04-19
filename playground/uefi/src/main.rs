fb6383629297fe943e01bf4f3f02b72d26df4690
matthew <matthew@hattmo.com>
2024-04-18 20:43:56 -0400

    delete wifihotspot

--- playground/uefi/src/main.rs
+++ playground/uefi/src/main.rs
@@ -1,29 +1,26 @@
 #![no_main]
 #![no_std]
 
-use log::info;
-use uefi::{
-    prelude::*,
-    proto::media::file::{File, FileAttribute, FileMode},
-    CStr16,
-};
-static HELLO: &str = "hello";
+use log::{info, set_logger};
+use uefi::logger::Logger;
+use uefi::prelude::*;
+use uefi_services::{print, println};
+
+static LOGGER: Logger = Logger::new();
 
 #[entry]
 fn main(image_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
-    let mut string_buf = [0u16; 100];
     uefi_services::init(&mut system_table).unwrap();
-    system_table.boot_services().stall(10_000_000);
-    let mut file_sys = system_table
-        .boot_services()
-        .get_image_file_system(image_handle)
-        .unwrap();
-    let c16_hello = CStr16::from_str_with_buf(HELLO, &mut string_buf[0..HELLO.len()]).unwrap();
-    let mut file = file_sys
-        .open_volume()
-        .unwrap()
-        .open(c16_hello, FileMode::CreateReadWrite, FileAttribute::empty())
-        .unwrap().into_regular_file().unwrap();
-    file.write("hello world".as_bytes()).unwrap();
+    println!("Hello world");
+    let stdout = system_table.stdout() as *mut _;
+    unsafe { LOGGER.set_output(stdout) };
+    //    set_logger(&LOGGER).inspect_err(|e|{
+    //        println!()int)
+    //    });
+
+    // info!("hello world");
+
+    let boot_svr = system_table.boot_services();
+    boot_svr.stall(1_000_000_000);
     Status::SUCCESS
 }
