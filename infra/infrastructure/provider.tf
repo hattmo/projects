ec7caeb23a23d0435bcdac73175fe538a56ceaae
Matthew Howard <matthew@hattmo.com>
2024-09-29 19:34:59 -0400

    remove infra

--- infra/infrastructure/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "vsphere" {
-  vsphere_server = var.vsphere_server
-  user           = var.vsphere_username
-  password       = var.vsphere_password
-
-  # If you have a self-signed cert
-  allow_unverified_ssl = true
-}
