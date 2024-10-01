b6dae31f5a5d4a5350360e658112e53e2c0fc2ea
Matthew Howard <matthew@hattmo.com>
2024-09-30 23:40:35 -0400

    clearing playground

--- playground/ptrace/src/test.c
+++
@@ -1,7 +0,0 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-    printf("hello world\n");
-}
--- playground/uefi/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-qemu-nbd -c /dev/nbd0 -f qcow2 disk.img
-partprobe /dev/nbd0
-sleep 1
-mount /dev/nbd0p1 ./disk
-cp ./target/x86_64-unknown-uefi/debug/uefi_test.efi ./disk/EFI/BOOT/BOOTx64.EFI
-umount ./disk
-qemu-nbd -d /dev/nbd0 > /dev/null
